a3fadf1baf912aec4b132899e3b4cfaf9033ef9d
Jelte Jansen <github@tjeb.nl>
2017-02-21 16:58:06 +0100

    add arp helper module

---
+++ lua/arp.lua
@@ -0,0 +1,48 @@
+--
+-- ARP lookup functions
+--
+
+local util = require 'util'
+
+local arp = {}
+
+-- arp table contents:
+-- <ip address> -> { 'hw': <mac address>, 'device': <iface> }
+
+function arp:get_hw_address(ip_address)
+  -- load if not loaded, or if ip is not in currently loaded table
+  if not arp.arptable or not arp.arptable[ip_address] then
+    arp.arptable = util:get_arp_table()
+  end
+  if arp.arptable[ip_address] then
+    return arp.arptable[ip_address]
+  else
+    return nil
+  end
+end
+
+function arp:get_ip_addresses(hw_address)
+  local result = {}
+  if not arp.arptable or not arp.arptable[ip_address] then
+    arp.arptable = util:get_arp_table()
+  end
+  for k,v in pairs(arp.arptable) do
+    if v == hw_address then
+      table.insert(result, k)
+    end
+  end
+  return result
+end
+
+function arp:print_table()
+  print("ARP TABLE:")
+  if not arp.arptable then
+    print("<not read>")
+  else
+    for k,v in pairs(arp.arptable) do
+      print(k .. ": " .. v)
+    end
+  end
+end
+
+return arp;
