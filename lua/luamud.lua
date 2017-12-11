51e0a2628b19e1705a38e3595f80c7195d090379
Jelte Jansen <github@tjeb.nl>
2017-12-11 17:53:06 +0100

    start with parsing acls

--- lua/luamud.lua
+++ lua/luamud.lua
@@ -8,6 +8,14 @@ cjson = require 'cjson'
 local luamud = {}
 
 -- MUD description encapsulation
+-- mud.data will contain the raw MUD (ietf-mud:mud) data
+-- mud.acls will contain the encapsulated access lists defined
+-- on the top-level of the MUD description. Note that these
+-- can in theory contain more than defined in the actual policies
+-- that are set
+--
+-- This implementation follows
+-- https://tools.ietf.org/html/draft-ietf-opsawg-mud-13
 local mud = {}
 mud.__index = mud
 
@@ -17,7 +25,8 @@ function luamud.mud_create_from_file(file)
     local f = assert(io.open(file, "rb"))
     local content = f:read("*all")
     f:close()
-    return luamud.mud_create(content)
+    new_mud, err = luamud.mud_create(content)
+    return new_mud, err
 end
 
 function luamud.mud_create(mud_json)
@@ -25,10 +34,33 @@ function luamud.mud_create(mud_json)
     setmetatable(new_mud, mud)
     mud.data, err = cjson.decode(mud_json)
     if mud.data == nil then return nil, err end
+
+    -- read the acls from ietf-access-control-list:access-lists
+    acl_count, err = new_mud:read_acls()
+    if acl_count == nil then return nil, err end
+
     mud, err = mud:validate()
     if mud == nil then return nil, err end
-    -- if we need to fill in extra values (last-update?)
     return mud
+end
+
+-- returns the number of acls read, or nil,error
+function mud:read_acls()
+    self.acls = {}
+    if self.data["ietf-access-control-list:access-lists"] == nil then
+        -- just leave empty or error?
+        return nil, "Error, no element 'ietf-access-control-list:access-lists'"
+    end
+    local acl_list = self.data["ietf-access-control-list:access-lists"]["acl"]
+    if acl_list == nil then
+        return nil, "Error, no element 'acl' in ietf-access-control-list:access-lists"
+    end
+    for _, acl_data in pairs(acl_list) do
+        acl_entry, err = luamud.acl_create(acl_data)
+        if acl_entry == nil then return nil, err end
+        table.insert(self.acls, acl_entry)
+    end
+    return table.getn(self.acls)
 end
 
 -- Validates and returns self if valid
@@ -54,6 +86,9 @@ function mud:validate()
     end
     -- systeminfo is optional and has no default
     -- extensions is optional and has no default (should we set empty list?)
+
+    -- check if the acls in the policies are actually defined
+
 
     --return "validation not implemented"
     return self
@@ -82,8 +117,79 @@ function mud:get_is_supported()
     return self.data["ietf-mud:mud"]["is-supported"]
 end
 
+function mud:get_systeminfo()
+    if self.data["ietf-mud:mud"]["systeminfo"] ~= nil then
+        return self.data["ietf-mud:mud"]["systeminfo"]
+    else
+        return nil
+    end
+end
+
+function mud:get_acl(name)
+end
+
 function mud:to_json()
     return cjson.encode(self.data)
 end
+
+
+--
+-- ACL encapsulation
+-- perhaps we should move this to its own module
+--
+-- This is the element of which a list is defined in
+-- ietf-access-control-list:access-lists/acl
+--
+-- This implementation follows
+-- https://tools.ietf.org/html/draft-ietf-opsawg-mud-13
+-- which extends the model at
+-- https://tools.ietf.org/html/draft-ietf-netmod-acl-model-14
+local acl = {}
+acl.__index = acl
+
+-- Create an acl structure using raw objects (ie the json data that
+-- has already been decoded)
+-- The data object is the element that is defined with the name
+-- ietf-access-control-list:access-lists
+function luamud.acl_create(data)
+    local new_acl = {}
+    setmetatable(new_acl, acl)
+    acl.data = data
+    acl.entries = {}
+    acl, err = acl:validate()
+    if acl == nil then return nil, err end
+    -- if we need to fill in extra values (last-update?)
+    return acl
+end
+
+function acl:validate()
+    -- the top-level element should be "acl"
+    print("[XX] validating ACL")
+    if self.data["acl-name"] == nil then
+        return nil, "No element 'acl-name' in access control list"
+    end
+    if self.data["acl-type"] == nil then
+        return nil, "No element 'acl-type' in access control list"
+    end
+    -- todo: check type against enum
+    local entries = self.data["access-list-entries"]
+    if entries == nil then
+        return nil, "No element 'access-list-entries' in access control list"
+    end
+    local ace = entries["ace"]
+    if entries == nil then
+        return nil, "No element 'ace' in access-list-entries"
+    end
+    for _, ace_e in pairs(ace) do
+        -- todo: should this be its own object as well?
+        table.insert(self.entries, ace_e)
+    end
+    --if self.data
+    return self
+end
+--
+-- end of ACL encapsulation
+--
+
 
 return luamud
--- lua/tests/test_mud.lua
+++ lua/tests/test_mud.lua
@@ -5,14 +5,19 @@ local luamud = require 'luamud'
 local mud, err = luamud.mud_create_from_file("tests/test.json")
 if mud == nil then
     print("Error: " .. err)
-end
-print(mud:to_json())
-print("MUD URL: " .. mud:get_mud_url())
-print("Last update: " .. mud:get_last_update())
-print("Cache validity: " .. mud:get_cache_validity())
-if mud:get_is_supported() then
-    print("Supported: Yes")
 else
-    print("Supported: No")
+    print(mud:to_json())
+    print("MUD URL: " .. mud:get_mud_url())
+    print("Last update: " .. mud:get_last_update())
+    print("Cache validity: " .. mud:get_cache_validity())
+    if mud:get_is_supported() then
+        print("Supported: Yes")
+    else
+        print("Supported: No")
+    end
+    if mud:get_systeminfo() then
+        print("Systeminfo: " .. mud:get_systeminfo())
+    else
+        print("Systeminfo not set")
+    end
 end
-
