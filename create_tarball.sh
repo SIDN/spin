1d38d3b2af40aaeb0df60ab159e1c1c8d2b85f3a
Jelte Jansen <jelte.jansen@sidn.nl>
2018-01-19 16:09:54 +0100

    add a 'no-check' option to create_tarball

--- create_tarball.sh
+++ create_tarball.sh
@@ -4,10 +4,29 @@ VERSION=`cat VERSION`
 
 BNAME="${PACKAGE}-${VERSION}"
 
-mkdir -p /tmp/${BNAME} &&\
-cp -r * /tmp/${BNAME}/ &&\
-(cd /tmp/${BNAME}; autoreconf --install && ./configure && make && make distclean && rm -rf lua/tests && rm -rf src/tests) &&\
-(cd /tmp; tar -czvf ${BNAME}.tar.gz ${BNAME}) &&\
-echo "Created /tmp/${BNAME}.tar.gz" &&\
-rm -rf /tmp/${BNAME} &&\
-md5sum /tmp/${BNAME}.tar.gz
+CHECK=1
+OPTION=$1
+if [ "$OPTION" = "-n" ]; then
+    CHECK=0
+fi
+
+if [ $CHECK -eq 0 ]; then
+
+    mkdir -p /tmp/${BNAME} &&\
+    cp -r * /tmp/${BNAME}/ &&\
+    (cd /tmp/${BNAME}; (make distclean||/bin/true)) &&\
+    (cd /tmp; tar -czvf ${BNAME}.tar.gz ${BNAME}) &&\
+    echo "Created /tmp/${BNAME}.tar.gz" &&\
+    rm -rf /tmp/${BNAME}
+
+else
+
+    mkdir -p /tmp/${BNAME} &&\
+    cp -r * /tmp/${BNAME}/ &&\
+    (cd /tmp/${BNAME}; autoreconf --install && ./configure && make && make distclean && rm -rf lua/tests && rm -rf src/tests) &&\
+    (cd /tmp; tar -czvf ${BNAME}.tar.gz ${BNAME}) &&\
+    echo "Created /tmp/${BNAME}.tar.gz" &&\
+    rm -rf /tmp/${BNAME} &&\
+    sha256sum /tmp/${BNAME}.tar.gz
+
+fi
