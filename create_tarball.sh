33eccbf86df4ac0041e106fb7d093933148cd56c
Jelte Jansen <github@tjeb.nl>
2018-01-26 22:09:37 +0100

    add autoreconf in create_tarball -n

--- create_tarball.sh
+++ create_tarball.sh
@@ -14,10 +14,11 @@ if [ $CHECK -eq 0 ]; then
 
     mkdir -p /tmp/${BNAME} &&\
     cp -r * /tmp/${BNAME}/ &&\
-    (cd /tmp/${BNAME}; (make distclean||/bin/true)) &&\
+    (cd /tmp/${BNAME}; autoreconf --install && (make distclean||/bin/true)) &&\
     (cd /tmp; tar -czvf ${BNAME}.tar.gz ${BNAME}) &&\
     echo "Created /tmp/${BNAME}.tar.gz" &&\
     rm -rf /tmp/${BNAME}
+    sha256sum /tmp/${BNAME}.tar.gz
 
 else
 
