3be2a289deed2b106c8ccad538c30d1d27a6131c
Jelte Jansen <github@tjeb.nl>
2018-05-09 10:36:45 +0200

    Use unbuffered output so the download prompt will appear immediately

--- web_ui/spin_webui.lua
+++ web_ui/spin_webui.lua
@@ -235,7 +235,7 @@ function tcpdumper.create(device, response)
     td.bytes_sent = 0
     td.response = response
 
-    local subp, err = mt_io.subprocess("tcpdump", {"-i", "br-lan", "-s", "1600", "-w", "-", "ether", "host", device}, 0, true, false, false)
+    local subp, err = mt_io.subprocess("tcpdump", {"-U", "-i", "br-lan", "-s", "1600", "-w", "-", "ether", "host", device}, 0, true, false, false)
     if subp == nil then
         return nil
     end
@@ -244,37 +244,46 @@ function tcpdumper.create(device, response)
     return td
 end
 
+function tcpdumper:read_and_send(size)
+    line, err = self.subp:read_bytes(size)
+    if line == nil then
+        if err ~= "read timed out" then
+            print("Error reading from subprocess: " .. err)
+            sent, err = response:send_chunk("")
+            subp:kill()
+            subp:close()
+            return nil, err
+        end
+    else
+        sent, err = self.response:send_chunk(line)
+        if sent == nil then
+            sent, err = self.response:send_chunk("")
+            print("Error sending data: " .. err)
+            subp:kill()
+            subp:close()
+            return nil, err
+        else
+            -- do not count the \r\n that signals end of chunk
+            self.bytes_sent = self.bytes_sent + sent - 2
+            return sent - 2
+        end
+    end
+end
+
 function tcpdumper:run()
     while self.running do
+        self:read_and_send(1600)
         copas.sleep(0.1)
-        line, err = self.subp:read_bytes(1024)
-        if line == nil then
-            print("[XX] error reading from subprocess: " .. err)
-            if err ~= "read timed out" then
-                sent, err = response:send_chunk("")
-                subp:kill()
-                subp:close()
-                return
-            end
-        else
-            sent, err = self.response:send_chunk(line)
-            if sent == nil then
-                sent, err = self.response:send_chunk("")
-                print("Error sending data: " .. err)
-                subp:kill()
-                subp:close()
-                return nil
-            else
-                -- do not count the \r\n that signals end of chunk
-                self.bytes_sent = self.bytes_sent + sent - 2
-            end
-        end
     end
     self.subp:kill()
     self.subp:close()
+
+    -- End with an empty chunk, as per transfer-encoding: chunked protocol
     sent, err = self.response:send_chunk("")
     if sent == nil then
         print("Error sending data: " .. err)
+    else
+        print("Sent " .. " bytes");
     end
     -- just to make sure
     self.running = false
