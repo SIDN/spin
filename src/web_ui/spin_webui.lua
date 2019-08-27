b05633c6342be555b44ed8169f3f48f27f910cae
Jelte Jansen <github@tjeb.nl>
2019-08-27 08:28:19 +0200

    Add uploader functionality to capture page

--- src/web_ui/spin_webui.lua
+++ src/web_ui/spin_webui.lua
@@ -235,6 +235,7 @@ function handler:handle_capture_manage(request, response)
 
     html, err = self:render_raw("mqtt.html", { device_name=device_name, device_mac=device_mac, device_ips=device_ips})
     response:set_header("Last-Modified", spin_util.get_time_string())
+    response:set_header("Access-Control-Allow-Origin", "spin.sidnlabs.nl")
 
     if html == nil then
         response:set_status(500, "Internal Server Error")
