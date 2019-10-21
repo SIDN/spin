23fc21e68b1435fa6c3e253aea8d55f2e6354979
Jelte Jansen <jelte.jansen@sidn.nl>
2019-10-21 10:56:09 +0200

    further handling of dots packet

--- src/spind/dots.c
+++ src/spind/dots.c
@@ -19,6 +19,25 @@
 #include "dots.h"
 #include "spin_log.h"
 
+#define MAX_ERR_LEN 256
+static char error_message[MAX_ERR_LEN];
+
+// Get the child object with the given name
+// return 0 on success, >0 on error, at which point error_message will be set
+int get_object(spin_data parent, spin_data* result, const char* name, int type) {
+    *result = cJSON_GetObjectItemCaseSensitive(parent, name);
+    if (*result == NULL) {
+        snprintf(error_message, MAX_ERR_LEN, "Missing field %s in %s", name, parent->string);
+        return 1;
+    }
+    if ((*result)->type != type) {
+        snprintf(error_message, MAX_ERR_LEN, "Wrong type of field %s in %s, found %d, expected %d", name, parent->string, (*result)->type, type);
+        return 2;
+    }
+
+    return 0;
+}
+
 
 int process_dots_signal(node_cache_t* node_cache, spin_data dots_message, char** error) {
     char* str;
@@ -27,13 +46,44 @@ int process_dots_signal(node_cache_t* node_cache, spin_data dots_message, char**
     str = cJSON_Print(dots_message);
     spin_log(LOG_DEBUG, "Message: %s\n", str);
     free(str);
-    spin_data mitigation_request = cJSON_GetObjectItemCaseSensitive(dots_message, "ietf-dots-signal-channel:mitigation-scope");
-
-    if (mitigation_request == NULL) {
-        *error = "ietf-dots-signal-channel:mitigation-scope missing";
-        return 2;
+    spin_data mitigation_request;
+    if (get_object(dots_message, &mitigation_request, "ietf-dots-signal-channel:mitigation-scope", cJSON_Object) != 0) {
+        *error = error_message;
+        return 1;
+    }
+    spin_data scope;
+    if (get_object(mitigation_request, &scope, "scope", cJSON_Array) != 0) {
+        *error = error_message;
+        return 1;
+    }
+    // for each element in scope, check if a device matches
+    for (int i = 0 ; i < cJSON_GetArraySize(scope) ; i++) {
+        cJSON* scope_item = cJSON_GetArrayItem(scope, i);
+        spin_data target_prefix;
+        spin_data source_prefix;
+        spin_data lifetime;
+        if (get_object(scope_item, &target_prefix, "target-prefix", cJSON_Array) != 0) {
+            *error = error_message;
+            return 1;
+        }
+        spin_log(LOG_DEBUG, "Target prefix check not implemented yet\n");
+        if (get_object(scope_item, &source_prefix, "ietf-dots-call-home:source-prefix", cJSON_Array) != 0) {
+            *error = error_message;
+            return 1;
+        }
+        if (get_object(scope_item, &lifetime, "lifetime", cJSON_Number) != 0) {
+            *error = error_message;
+            return 1;
+        }
+        // TODO: What to do with lifetime? (if anything)
+        for (int j = 0; j < cJSON_GetArraySize(target_prefix); j++) {
+            //spin_data c_prefix = cJSON_GetArrayItem(target_prefix, j);
+            const char* prefix = cJSON_GetArrayItem(target_prefix, j)->valuestring;
+            spin_log(LOG_DEBUG, "Checking prefix: %s\n", prefix);
+            // Loop over all devices' recent history entries
+        }
     }
 
-    *error = "Not implemented";
+    *error = "Not implemented yet";
     return 1;
 }
