f85d024c15d39e870867c872ea36599147cb7dc5
Caspar Schutijser <caspar.schutijser@sidn.nl>
2019-11-27 11:18:08 +0100

    spin-pcap-reader: must include some more header files

--- src/tools/spin-pcap-reader/nodes.c
+++ src/tools/spin-pcap-reader/nodes.c
@@ -1,5 +1,10 @@
 #include "config.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <net/if.h>
+#include <netinet/in.h>
 #ifdef HAVE_NET_ETHERNET_H
 #include <net/ethernet.h>
 #endif
