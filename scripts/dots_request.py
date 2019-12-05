ced0415ab6357f01e69b0c9e9bd054f99abff96e
Jelte Jansen <github@tjeb.nl>
2019-12-05 13:48:58 +0100

    Initial version of dots request client tool

---
+++ scripts/dots_request.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+# Copyright SIDN Labs 2019
+# License: GPL, see the LICENSE file for more information
+
+#
+# This is a small script that creates DOTS signal home mitigation requests
+#
+
+import argparse
+import ipaddress
+import json
+import requests
+import sys
+
+class Prefix(object):
+    def __init__(self, prefix_str):
+        self.prefix_str = prefix_str
+        self.prefix = ipaddress.ip_network(prefix_str)
+
+    def validate(self):
+        pass
+
+    def __str__(self):
+        return self.prefix_str
+
+class MitigationRequest(object):
+    def __init__(self):
+        self.lifetime = 3600
+        self.target_prefixes = []
+        self.source_prefixes = []
+
+    def add_target_prefix(self, prefix):
+        self.target_prefixes.append(Prefix(prefix))
+
+    def add_source_prefix(self, prefix):
+        self.source_prefixes.append(Prefix(prefix))
+
+    def set_lifetime(self, lifetime):
+        self.lifetime = lifetime
+
+    def as_obj(self):
+        scope = {
+            "lifetime": self.lifetime
+        }
+
+        if self.source_prefixes:
+            scope['ietf-dots-call-home:source-prefix'] = [str(p) for p in self.source_prefixes]
+
+        if self.target_prefixes:
+            scope['target-prefix'] = [str(p) for p in self.target_prefixes],
+
+        result = {
+            "ietf-dots-signal-channel:mitigation-scope": {
+                "scope": [ scope ]
+            }
+        }
+        return result
+
+class RPCSender(object):
+    def __init__(self, url=None):
+        if url is None:
+            self.url = "http://192.168.8.1/spin_api/jsonrpc"
+        else:
+            self.url = url
+
+    def create_jsonrpc_request(self, mitigation_request):
+        result = {
+            'method': 'dots-signal',
+            'params': {
+                'dots_signal': mitigation_request.as_obj()
+            }
+        }
+
+        return result
+
+    def send(self):
+        #requests.
+        pass
+
+def main(args):
+    mr = MitigationRequest()
+    if args.source:
+        for prefix in args.source:
+            try:
+                mr.add_source_prefix(prefix)
+            except ValueError as ve:
+                print("Bad prefix: %s" % str(ve))
+                sys.exit(1)
+    if args.target:
+        for prefix in args.target:
+            try:
+                mr.add_target_prefix(prefix)
+            except ValueError as ve:
+                print("Bad prefix: %s" % str(ve))
+                sys.exit(1)
+
+    if args.print:
+        print(json.dumps(mr.as_obj(), indent=2))
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-s', '--source', type=str, action='append', default=[], help='Source prefix')
+    parser.add_argument('-t', '--target', type=str, action='append', default=[], help='Target prefix')
+    parser.add_argument('-p', '--print', action="store_true", help="Print the request as json")
+
+    args = parser.parse_args()
+    main(args)
+
+
+SAMPLE = """
+     {
+       "ietf-dots-signal-channel:mitigation-scope": {
+         "scope": [
+           {
+             "target-prefix": [
+                "2001:db8:c000::/128"
+              ],
+             "ietf-dots-call-home:source-prefix": [
+                "2001:db8:123::/128"
+              ],
+             "lifetime": 3600
+           }
+         ]
+       }
+     }
+"""
