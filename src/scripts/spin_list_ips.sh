072c80c1b88dd28c30b99703258281b44676dff2
Jelte Jansen <github@tjeb.nl>
2020-10-01 11:10:12 +0200

    Add arguments to spin_list_ips.sh

--- src/scripts/spin_list_ips.sh
+++ src/scripts/spin_list_ips.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 # List the IP addresses on this machine, one per line, no netmask
-ip addr show | grep inet | sed "s/^\s*//" | cut -f 2 -d ' ' | cut -f 1 -d '/'
+force=
+outfile=
+while getopts "fho:" opt; do
+  case $opt in
+    h)
+    echo "Usage: foobar"
+    exit
+    ;;
+    f) force="yes"
+    ;;
+    o) outfile="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+RESULT=`ip addr show | grep inet | sed "s/^\s*//" | cut -f 2 -d ' ' | cut -f 1 -d '/'`
+
+if [ -n "$outfile" ]; then
+    if [ -e "$outfile" ]; then
+        if [ -n "$force" ]; then
+            echo "${RESULT}" > ${outfile};
+        else
+            echo "File exists already, use -f to overwrite";
+        fi
+    else
+        echo "${RESULT}" > ${outfile};
+    fi
+else
+    echo "${RESULT}"
+fi
